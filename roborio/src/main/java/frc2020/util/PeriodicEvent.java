ec7a3477ad1e01cf56ee00ec9cd44f60a19bb5d6
JonathanREmery <JonathanREmery@gmail.com>
2020-02-07 18:44:56 -0500

    Fixed last time bug in PeriodicEventManager

--- roborio/src/main/java/frc2020/util/PeriodicEvent.java
+++ roborio/src/main/java/frc2020/util/PeriodicEvent.java
@@ -1,8 +1,18 @@
 package frc2020.util;
 
+/**
+ * Interface for an event that is to be run periodically
+ */
 public interface PeriodicEvent {
 
+    /**
+     * @return Whether to call run or not
+     */
     public boolean condition();
+
+    /**
+     * Method to run periodically 
+     */
     public void run();
 
 };
--- roborio/src/main/java/frc2020/util/PeriodicEventManager.java
+++ roborio/src/main/java/frc2020/util/PeriodicEventManager.java
@@ -5,38 +5,48 @@ import edu.wpi.first.wpilibj.Timer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Manages running all PeriodicEvents periodically
+ */
 public class PeriodicEventManager {
 
     private List<PeriodicEvent> events_ = new ArrayList<PeriodicEvent>();
     private List<Double> eventIntervals_ = new ArrayList<Double>();
-    
-    private double lastTime_ = 0.0;
+    private List<Double> lastTimes_ = new ArrayList<Double>();
 
-    public PeriodicEventManager() {
-        this.lastTime_ = Timer.getFPGATimestamp();
-    }
-
+    /**
+     * @param event Event to run periodically
+     * @param eventInterval Periodic interval to run event
+     */
     public void addEvent(PeriodicEvent event, double eventInterval) {
         events_.add(event);
         eventIntervals_.add(eventInterval);
+        lastTimes_.add(Timer.getFPGATimestamp());
     }
 
+    /**
+     * @param event Event to remove
+     */
     public void removeEvent(PeriodicEvent event) {
         for (int i = 0; i < events_.size(); i++) {
             if (events_.get(i) == event) {
                 events_.remove(i);
                 eventIntervals_.remove(i);
+                lastTimes_.remove(i);
             }
         }
     }
 
+    /**
+     * Handles periodic calling of all PeriodicEvent's code
+     */
     public void run() {
         for (int i = 0; i < events_.size(); i++){
-            if (Timer.getFPGATimestamp()-this.lastTime_ >= eventIntervals_.get(i)) {
+            if (Timer.getFPGATimestamp()-lastTimes_.get(i) >= eventIntervals_.get(i)) {
                 if (events_.get(i).condition()){
                     events_.get(i).run();
                 }
-                this.lastTime_ = Timer.getFPGATimestamp();
+                lastTimes_.set(i, Timer.getFPGATimestamp());
             }
         }
     }
