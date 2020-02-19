528ca7f46c53f9be0f11cbf43223aa8169353bb9
Alex Odom <alexjodom@gmail.com>
2020-02-19 11:29:01 -0500

    Added Limit Swich Support and Zeoring
    - Zeoring is disabled at the moment

--- roborio/src/main/java/frc2020/subsystems/Turret.java
+++ roborio/src/main/java/frc2020/subsystems/Turret.java
@@ -7,8 +7,12 @@ public class Turret extends SingleMotorSubsystem {
 
     private static Turret instance_;
 
+    private static double FORWARD_LIMIT_SWITCH_POSITION = 355;
+    private static double REVERSE_LIMIT_SWITCH_POSITION = 0;
+
     private final static SingleMotorSubsystemConstants DEFAULT_CONSTANTS =
             new SingleMotorSubsystemConstants();
+
     static {
         var masterConstants = new MotorConstants();
         masterConstants.id_ = 7;
@@ -45,19 +49,31 @@ public class Turret extends SingleMotorSubsystem {
 
     @Override
     protected boolean handleZeroing() {
+        // TODO: Enable zeroing once sensor units and limit swich posotion is found
+        final boolean enableZeroing = false;
+        if (enableZeroing) {
+            if (atForwardLimit()) {
+                encoder.setPosition(FORWARD_LIMIT_SWITCH_POSITION);
+                return true;
+            }
+            if (atReverseLimit()) {
+                encoder.setPosition(REVERSE_LIMIT_SWITCH_POSITION);
+                return true;
+            }
+        }
         return false;
     }
 
-    // TODO: Make sure sensor phase is correct (clockwise negative)
+    // TODO: Check that each limit switch is pluged in to it's
+    // correct forward or reverse limit switch port.
     @Override
     protected synchronized boolean atReverseLimit() {
-        return false;
+        return io_.reverseLimit;
     }
 
-    // TODO: Make sure sensor phase is correct (clockwise negative)
     @Override
     protected synchronized boolean atForwardLimit() {
-        return false;
+        return io_.forwardLimit;
     }
 
     @Override
