3734abfe735954c9719f7727575603263878883c
foodieme888 <asiangamerdowndastreet@gmail.com>
2021-02-27 12:25:41 -0500

    Changed code to be quintic spline

--- roborio-Imported/src/main/java/frc2020/auto/modes/TripleLoop.java
+++ roborio-Imported/src/main/java/frc2020/auto/modes/TripleLoop.java
@@ -2,7 +2,8 @@ package frc2020.auto.modes;
 
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
+//import edu.wpi.first.wpilibj.geometry.Translation2d;
+//Commented this out as this was changed from a cubic to a quintic spline
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
@@ -33,49 +34,50 @@ public class TripleLoop extends AutoMode {
         var maxAccel = 0.5; // m/s
         var maxVelocity = 0.5; // m/s
 
-        var startPose = new Pose2d();
-
         // This can be a list of Translation2d and Pose2d
         // Note if you use a Pose2d WPILib will
         // generate a quntic spline instead of a cubic
         // spline. (This may take longer)
+        var waypointStart = new Pose2d(0.0, 0.0, new Rotation2d());
 
-        var midPoint1 = new Translation2d(3.048, 0);
-        var midPoint2 = new Translation2d(3.429, -.762);
-        var midPoint3 = new Translation2d(3.048, -1.143);
-        var midPoint4 = new Translation2d(2.667, -.762);
-        var midPoint5 = new Translation2d(3.81, 0);
-        var midPoint6 = new Translation2d(4.953, 0);
-        var midPoint7 = new Translation2d(6.096, .762);
-        var midPoint8 = new Translation2d(5.334, 1.143);
-        var midPoint9 = new Translation2d(4.953, .762);
-        var midPoint10 = new Translation2d(4.953, 0);
-        var midPoint11 = new Translation2d(6.096, -1.143);
-        var midPoint12 = new Translation2d(6.858, -1.243);
-        var midPoint13 = new Translation2d(7.62, -.762);
-        var midPoint14 = new Translation2d(6.858, 0);
-        var midPoint15 = new Translation2d(4.953, 0);
-        var midPoint16 = new Translation2d(3.048, .381);
+        var waypoint1 = new Pose2d(3.048, 0, new Rotation2d());
+        var waypoint2 = new Pose2d(3.429, -.762, new Rotation2d(90));
+        var waypoint3 = new Pose2d(3.048, -1.143, new Rotation2d(180));
+        var waypoint4 = new Pose2d(2.667, -.762, new Rotation2d(270));
+        var waypoint5 = new Pose2d(3.81, 0, new Rotation2d());
+        var waypoint6 = new Pose2d(4.953, 0, new Rotation2d());
+        var waypoint7 = new Pose2d(6.096, .762, new Rotation2d(260));
+        var waypoint8 = new Pose2d(5.334, 1.143, new Rotation2d(170));
+        var waypoint9 = new Pose2d(4.953, .762, new Rotation2d(85));
+        var waypoint10 = new Pose2d(4.953, 0, new Rotation2d(45));
+        var waypoint11 = new Pose2d(6.096, -1.143, new Rotation2d(30));
+        var waypoint12 = new Pose2d(6.858, -1.243, new Rotation2d(30));
+        var waypoint13 = new Pose2d(7.62, -.762, new Rotation2d(225));
+        var waypoint14 = new Pose2d(6.858, 0, new Rotation2d(180));
+        var waypoint15 = new Pose2d(4.953, 0, new Rotation2d(190));
+        var waypoint16 = new Pose2d(3.048, .381, new Rotation2d(180));
 
-        var midPoints = new ArrayList<Translation2d>();
-        midPoints.add(midPoint1);
-        midPoints.add(midPoint2);
-        midPoints.add(midPoint3);
-        midPoints.add(midPoint4);
-        midPoints.add(midPoint5);
-        midPoints.add(midPoint6);
-        midPoints.add(midPoint7);
-        midPoints.add(midPoint8);
-        midPoints.add(midPoint9);
-        midPoints.add(midPoint10);
-        midPoints.add(midPoint11);
-        midPoints.add(midPoint12);
-        midPoints.add(midPoint13);
-        midPoints.add(midPoint14);
-        midPoints.add(midPoint15);
-        midPoints.add(midPoint16);
-
-        var endPose = new Pose2d(0.0, 1.524, Rotation2d.fromDegrees(0.0));
+        var wayPointEnd = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
+        
+        var waypoints = new ArrayList<Pose2d>();
+        waypoints.add(waypointStart);
+        waypoints.add(waypoint1);
+        waypoints.add(waypoint2);
+        waypoints.add(waypoint3);
+        waypoints.add(waypoint4);
+        waypoints.add(waypoint5);
+        waypoints.add(waypoint6);
+        waypoints.add(waypoint7);
+        waypoints.add(waypoint8);
+        waypoints.add(waypoint9);
+        waypoints.add(waypoint10);
+        waypoints.add(waypoint11);
+        waypoints.add(waypoint12);
+        waypoints.add(waypoint13);
+        waypoints.add(waypoint14);
+        waypoints.add(waypoint15);
+        waypoints.add(waypoint16);
+        waypoints.add(wayPointEnd);
         
         // Define other constraintes
         var voltageConstraint = 
@@ -91,7 +93,7 @@ public class TripleLoop extends AutoMode {
                 .addConstraint(voltageConstraint);
 
         autoLine = TrajectoryGenerator.generateTrajectory(
-            startPose, midPoints, endPose, config);
+            waypoints, config);
     }
 
     @Override
